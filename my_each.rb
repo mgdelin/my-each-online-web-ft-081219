340e86ff0ae54a2e2ea94b75da8899ea9ead1952
Mercedes Delin <mercedesgdelin@gmail.com>
2019-10-24 15:10:28 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each (collection)
+  counter = 0 
+  while collection.length > counter do 
+  collection.each |element|
+    return element
+    counter += 1
+  end
 end
